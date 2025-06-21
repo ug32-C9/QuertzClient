0ef12b2a60ca16c1e1b7459aa640185a93fabcd8
itzC9 <154809425+itzC9@users.noreply.github.com>
2025-06-21 08:22:37 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 
 main() {
     clear
-    echo -e "QuertzClient Mac Install Script - Begin"
+    echo -e "QuertzClientMac Install Script - Begin"
     curl -s "https://git.raptor.fun/main/jq-macos-amd64" -o "./jq"
     chmod +x ./jq
 
     echo -e "Downloading Latest Roblox..."
     [ -f ./RobloxPlayer.zip ] && rm ./RobloxPlayer.zip
-    local versionInfo=$(curl -s "https://raw.githubusercontent.com/itzC9/QuertzClientMac/refs/heads/main/version.json")
+    local versionInfo=$(curl -s "https://raw.githubusercontent.com/itzC9/QuertzClient/refs/heads/main/version.json")
     
     local mChannel=$(echo $versionInfo | ./jq -r ".channel")
     local version=$(echo $versionInfo | ./jq -r ".\"roblox-client\"")
@@ -23,15 +23,15 @@ main() {
     rm ./RobloxPlayer.zip
     echo -e "Done."
 
-    echo -e "Downloading QuertzClient Mac..."
-    curl "https://raw.githubusercontent.com/itzC9/QuertzClientMac/refs/heads/main/main/QuertzClient.zip" -o "./QuertzClient.zip"
+    echo -e "Downloading QuertzClientMac..."
+    curl "https://raw.githubusercontent.com/itzC9/QuertzClient/refs/heads/main/main/QuertzClient.zip" -o "./QuertzClient.zip"
 
-    echo -n "Installing QuertzClient Mac... "
+    echo -n "Installing QuertzClientMac... "
     unzip -o -q "./QuertzClient.zip"
     echo -e "Done."
 
     echo -n "Updating Dylib..."
-    curl -Os "https://raw.githubusercontent.com/itzC9/QuertzClientMac/refs/heads/main/$mChannel/libQuertzClient.dylib"
+    curl -Os "https://raw.githubusercontent.com/itzC9/QuertzClient/refs/heads/main/$mChannel/libQuertzClient.dylib"
     
     echo -e " Done."
     echo -e "Patching Roblox..."
@@ -43,12 +43,12 @@ main() {
 
     echo -n "Installing QuertzClient App... "
     [ -d "/Applications/QuertzClient.app" ] && rm -rf "/Applications/QuertzClient.app"
-    mv "./QuertzClient Mac.app" "/Applications/QuertzClient Mac.app"
+    mv "./QuertzClientMac.app" "/Applications/QuertzClientMac.app"
     rm ./QuertzClient.zip
     rm ./jq
 
     echo -e "Done."
-    echo -e "Install Complete! Created by Nexus42 and Frame!"
+    echo -e "Install Complete! QuertzClient Ready To Use!"
     exit
 }
 
